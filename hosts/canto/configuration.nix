01e50a884a4050412b401d58d64a200d1642d5a3
Teague M <65279644+tmangan64@users.noreply.github.com>
2026-01-30 13:46:52 +0000

    added #canto
    added 'canto' (thinkpad laptop)

---
+++ hosts/canto/configuration.nix
@@ -0,0 +1,575 @@
+Mizutani
+acethekell
+Invisible
+
+Mizutani
+
+ — 02/11/2025, 07:27
+Image
+Image
+Image
+Mizutani
+
+ — 02/11/2025, 07:51
+LxCxmw!$P3s7V2^
+Mizutani
+
+ — 02/11/2025, 14:34
+Image
+Image
+Mizutani
+
+ — 05/11/2025, 17:25
+Image
+https://www.smartsurvey.co.uk/s/HHO75F/
+MMU 05.11
+Please take the time to complete our survey. Your feedback is important.
+Image
+Mizutani
+
+ — 13/11/2025, 00:30
+Image
+Mizutani
+
+ — 02/12/2025, 17:10
+Image
+Mizutani
+
+ — 17/12/2025, 17:27
+Jellyfin/plex shi:
+Re:ZERO
+Attack on Titan
+Disaster Trilogy
+Heavenly Delusion
+Evangelion (rebuilds, original and eoe)
+Dan Da Dan
+Jujutsu Kaisen
+Mushoku Tensei
+Sword Art Online
+Call of the Night
+Frieren
+Edgerunners
+Death Note
+Demon Slayer
+Violet Evergarden
+Cowboy Bebop
+Chainsaw Man
+Mizutani
+
+ — 21/12/2025, 14:58
+server root qaw22VdaPSSvSnq
+Mizutani
+
+ — 27/12/2025, 14:41
+arp -a
+
+Interface: 169.254.159.161 --- 0x4
+  Internet Address      Physical Address      Type
+  169.254.255.255       ff-ff-ff-ff-ff-ff     static
+  224.0.0.2             01-00-5e-00-00-02     static
+  224.0.0.22            01-00-5e-00-00-16     static
+  224.0.0.251           01-00-5e-00-00-fb     static
+  224.0.0.252           01-00-5e-00-00-fc     static
+  224.0.0.253           01-00-5e-00-00-fd     static
+  239.255.255.250       01-00-5e-7f-ff-fa     static
+
+Interface: 172.16.0.171 --- 0xb
+  Internet Address      Physical Address      Type
+  172.16.0.1            48-a9-8a-fd-77-aa     dynamic
+  172.16.0.78           8c-70-5a-c9-9a-e8     dynamic
+  172.16.1.199          4c-3b-df-6b-67-62     dynamic
+  172.16.2.36           b6-08-22-64-82-a1     dynamic
+  172.16.2.53           1c-57-dc-7e-7a-a4     dynamic
+  172.16.3.255          ff-ff-ff-ff-ff-ff     static
+  224.0.0.2             01-00-5e-00-00-02     static
+  224.0.0.22            01-00-5e-00-00-16     static
+  224.0.0.251           01-00-5e-00-00-fb     static
+  224.0.0.252           01-00-5e-00-00-fc     static
+  224.0.0.253           01-00-5e-00-00-fd     static
+  239.255.255.250       01-00-5e-7f-ff-fa     static
+  255.255.255.255       ff-ff-ff-ff-ff-ff     static
+
+Interface: 192.168.56.1 --- 0x13
+  Internet Address      Physical Address      Type
+  192.168.56.255        ff-ff-ff-ff-ff-ff     static
+  224.0.0.22            01-00-5e-00-00-16     static
+
+  239.255.255.250       01-00-5e-7f-ff-fa     static
+
+Interface: 10.5.0.2 --- 0x17
+  Internet Address      Physical Address      Type
+  10.5.255.255                                static
+  224.0.0.22                                  static
+  224.0.0.251                                 static
+  224.0.0.252                                 static
+  224.0.0.253                                 static
+  239.255.255.250                             static
+Mizutani
+
+ — 27/12/2025, 14:49
+Ethernet adapter Ethernet:
+
+   Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::c341:af9b:9e35:98ce%4
+   Autoconfiguration IPv4 Address. . : 169.254.159.161
+   Subnet Mask . . . . . . . . . . . : 255.255.0.0
+   Default Gateway . . . . . . . . . :
+Ethernet adapter Ethernet:
+
+   Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::c341:af9b:9e35:98ce%4
+   IPv4 Address. . . . . . . . . . . : 192.168.137.1
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . :
+alt — 27/12/2025, 15:29
+Set-NetConnectionProfile -Name "NetworkName" -NetworkCategory Private
+alt — 03/01/2026, 10:53
+I'm lowjey jus your lil storage
+Mizutani
+
+ — 03/01/2026, 22:17
+esmBot
+APP
+ — 03/01/2026, 22:17
+Image
+Mizutani
+
+ — 04/01/2026, 17:26
+Image
+Mizutani
+ started a call that lasted a few seconds. — 04/01/2026, 23:49
+alt — 07/01/2026, 12:20
+Attachment file type: code
+Data types in R.R
+622 bytes
+Attachment file type: code
+Factor in R.R
+214 bytes
+Attachment file type: code
+Matrix function in R.R
+603 bytes
+Mizutani
+
+ — 10/01/2026, 14:31
+Image
+Image
+Mizutani
+
+ — 10/01/2026, 17:53
+Image
+Image
+Image
+Image
+Image
+Image
+Image
+alt — 10/01/2026, 19:40
+Image
+alt — 10/01/2026, 19:51
+Image
+Image
+alt — 10/01/2026, 20:31
+Attachment file type: unknown
+CamCapture1.pcapng
+7.97 MB
+Attachment file type: unknown
+Capture1.pcapng
+4.41 MB
+Attachment file type: unknown
+Capture2.pcapng
+1.50 KB
+Attachment file type: unknown
+Capture3.pcapng
+7.46 MB
+Attachment file type: unknown
+Capture4.pcapng
+4.09 MB
+Attachment file type: unknown
+Capture5.pcapng
+8.14 MB
+Attachment file type: unknown
+init capture.pcapng
+189.74 KB
+Attachment file type: unknown
+JECapture1.pcapng
+5.97 MB
+Attachment file type: unknown
+JECapture2.pcapng
+6.83 MB
+Attachment file type: unknown
+JECapture3.pcapng
+4.86 MB
+Mizutani
+
+ — 10/01/2026, 22:27
+Filtering for 'beacon frames'
+Image
+Beacon frames + my wifi network's name
+Image
+The MAC addresses of my house are:
+AA:95:7F:2D:AE:AC
+AA:95:7F:2D:AD:38
+AA:95:7F:2D:AB:34
+Mizutani
+
+ — 10/01/2026, 22:52
+Found a MAC address that Wireshark's local OUI registry resolved to Google
+Image
+Mizutani
+
+ — 11/01/2026, 01:37
+What we can see here:
+Periodic data from camera
+Around packet 25k, I power cycled the camera
+We can see probe requests as the device attempts to connect to the internet again followed by data as I am actively viewing the camera via the app
+Image
+Mizutani
+
+ — 13/01/2026, 21:08
+Image
+Mizutani
+
+ — 13/01/2026, 23:19
+Image
+Mizutani
+
+ — 14/01/2026, 19:57
+Image
+Mizutani
+
+ — 14/01/2026, 23:51
+Image
+Mizutani
+
+ — 19/01/2026, 22:56
+Image
+Mizutani
+
+ — 19/01/2026, 23:08
+Image
+Mizutani
+
+ — 19/01/2026, 23:26
+Image
+Mizutani
+
+ — 22/01/2026, 03:12
+Image
+esmBot
+APP
+ — 22/01/2026, 20:14
+Image
+esmBot
+APP
+ — 24/01/2026, 13:53
+Image
+Mizutani
+
+ — 24/01/2026, 22:40
+Done! Congratulations on your new bot. You will find it at t.me/RacheBartmoss_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+
+Use this token to access the HTTP API:
+8470318862:AAEjQMdW9YMurwHSv5uu9cmdJ9F_HuSc76c
+Keep your token secure and store it safely, it can be used by anyone to control your bot.
+
+For a description of the Bot API, see this page: https://core.telegram.org/bots/api
+Telegram Bot API
+The Bot API is an HTTP-based interface created for developers keen on building bots for Telegram.
+To learn how to create…
+Mizutani
+
+ — 26/01/2026, 01:48
+To be totally honest I wasn't even going to respond but I found a screenshot of the last time you've pulled this shit and I cannot believe I still trusted you after the first time. I cannot count how many times you've told me you're some innocent bystander only to go ahead and tell someone what I've said behind my back. You did it with Allicia with the tents and you've done it with Anya countless times. I felt like I could trust you and vent to you to get something off my chest but you took everything I said literally and used it as ammo to fuel this shit. I never should have trusted you again after the first time and especially after I explained how Jamie did the exact same shit and you still went ahead and did it. Every single time you told someone something I said in confidence, you made it infinitely worse. I just cannot understand it at all man. I'm so fucking disappointed in you.
+alt — Yesterday at 20:39
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+configuration.nix
+5 KB
+{
+  "nodes": {
+    "home-manager": {
+      "inputs": {
+        "nixpkgs": [
+          "nixpkgs"
+
+flake.lock
+2 KB
+{
+    description = "NixOS config with Home Manager via flakes";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+
+flake.nix
+1 KB
+# Do not modify this file!  It was generated by ‘nixos-generate-config’
+# and may be overwritten by future invocations.  Please make changes
+# to /etc/nixos/configuration.nix instead.
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+
+hardware-configuration.nix
+2 KB
+{
+    home.stateVersion = "25.05";
+}
+home.nix
+1 KB
+Mizutani
+
+ — Yesterday at 22:12
+Here is the token for bot Rache @RacheBartmoss_bot:
+
+8470318862:AAEjQMdW9YMurwHSv5uu9cmdJ9F_HuSc76c
+alt — 13:30
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+configuration.nix
+5 KB
+{
+  "nodes": {
+    "home-manager": {
+      "inputs": {
+        "nixpkgs": [
+          "nixpkgs"
+
+flake.lock
+2 KB
+{
+    description = "NixOS config with Home Manager via flakes";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+
+flake.nix
+1 KB
+# Do not modify this file!  It was generated by ‘nixos-generate-config’
+# and may be overwritten by future invocations.  Please make changes
+# to /etc/nixos/configuration.nix instead.
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+
+hardware-configuration.nix
+2 KB
+{
+    home.stateVersion = "25.05";
+}
+home.nix
+1 KB
+﻿
+alt
+basedgroovy
+him
+ 
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.graphics.enable = true;
+
+  networking.hostName = "elysia"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+
+  # Set your time zone.
+  time.timeZone = "Europe/Jersey";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true; 
+  programs.hyprland.enable = false;
+ 
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.flatpak.enable = true;
+  #disable geoclue cause causing issues, will fix later
+  services.geoclue2.enable = false;
+
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kiroshi = {
+    isNormalUser = true;
+    description = "Teague Mangan";
+    extraGroups = [ "networkmanager" "wheel" "vboxusers" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+    shell = pkgs.fish;
+  };
+
+  # Install programs
+  programs.firefox.enable = true;
+  programs.steam.enable = true;
+
+  #fish
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+
+  
+  services.fprintd.enable = false;
+  security.pam.services.gdm.enable = true;
+  security.pam.services.login.enable = true;
+  security.pam.services.sudo.enable = true;
+
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    kitty
+    fastfetch
+	  hyprland
+	  spotify
+	  speedtest-cli
+	  git
+	  discord
+	  gnome-tweaks
+	  nodejs_24
+	  vscode
+    lynx
+    obsidian
+
+    rWrapper
+    rstudio
+
+    wireshark-qt
+
+    code-cursor
+
+    ani-cli
+
+	#forgot why
+	  python314
+
+	#mdbook for NixOS guide
+	  mdbook
+
+	#desktop widget
+	  conky
+    gnomeExtensions.vitals
+    gnomeExtensions.extension-list
+    gnomeExtensions.todo
+
+	#godot
+	  godot
+
+	#github
+	  github-desktop
+
+	#n8n
+	  n8n
+
+	#p7zip
+    gnumake
+  ];
+
+  virtualisation.virtualbox.host.enable = true;
+  boot.blacklistedKernelModules = [ "kvm_intel" "kvm" ];
+
+
+  nix = {
+    package = pkgs.nix;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ hosts/canto/hardware-configuration.nix
@@ -0,0 +1,418 @@
+Mizutani
+acethekell
+Invisible
+
+Mizutani
+
+ — 02/11/2025, 07:27
+Image
+Image
+Image
+Mizutani
+
+ — 02/11/2025, 07:51
+LxCxmw!$P3s7V2^
+Mizutani
+
+ — 02/11/2025, 14:34
+Image
+Image
+Mizutani
+
+ — 05/11/2025, 17:25
+Image
+https://www.smartsurvey.co.uk/s/HHO75F/
+MMU 05.11
+Please take the time to complete our survey. Your feedback is important.
+Image
+Mizutani
+
+ — 13/11/2025, 00:30
+Image
+Mizutani
+
+ — 02/12/2025, 17:10
+Image
+Mizutani
+
+ — 17/12/2025, 17:27
+Jellyfin/plex shi:
+Re:ZERO
+Attack on Titan
+Disaster Trilogy
+Heavenly Delusion
+Evangelion (rebuilds, original and eoe)
+Dan Da Dan
+Jujutsu Kaisen
+Mushoku Tensei
+Sword Art Online
+Call of the Night
+Frieren
+Edgerunners
+Death Note
+Demon Slayer
+Violet Evergarden
+Cowboy Bebop
+Chainsaw Man
+Mizutani
+
+ — 21/12/2025, 14:58
+server root qaw22VdaPSSvSnq
+Mizutani
+
+ — 27/12/2025, 14:41
+arp -a
+
+Interface: 169.254.159.161 --- 0x4
+  Internet Address      Physical Address      Type
+  169.254.255.255       ff-ff-ff-ff-ff-ff     static
+  224.0.0.2             01-00-5e-00-00-02     static
+  224.0.0.22            01-00-5e-00-00-16     static
+  224.0.0.251           01-00-5e-00-00-fb     static
+  224.0.0.252           01-00-5e-00-00-fc     static
+  224.0.0.253           01-00-5e-00-00-fd     static
+  239.255.255.250       01-00-5e-7f-ff-fa     static
+
+Interface: 172.16.0.171 --- 0xb
+  Internet Address      Physical Address      Type
+  172.16.0.1            48-a9-8a-fd-77-aa     dynamic
+  172.16.0.78           8c-70-5a-c9-9a-e8     dynamic
+  172.16.1.199          4c-3b-df-6b-67-62     dynamic
+  172.16.2.36           b6-08-22-64-82-a1     dynamic
+  172.16.2.53           1c-57-dc-7e-7a-a4     dynamic
+  172.16.3.255          ff-ff-ff-ff-ff-ff     static
+  224.0.0.2             01-00-5e-00-00-02     static
+  224.0.0.22            01-00-5e-00-00-16     static
+  224.0.0.251           01-00-5e-00-00-fb     static
+  224.0.0.252           01-00-5e-00-00-fc     static
+  224.0.0.253           01-00-5e-00-00-fd     static
+  239.255.255.250       01-00-5e-7f-ff-fa     static
+  255.255.255.255       ff-ff-ff-ff-ff-ff     static
+
+Interface: 192.168.56.1 --- 0x13
+  Internet Address      Physical Address      Type
+  192.168.56.255        ff-ff-ff-ff-ff-ff     static
+  224.0.0.22            01-00-5e-00-00-16     static
+
+  239.255.255.250       01-00-5e-7f-ff-fa     static
+
+Interface: 10.5.0.2 --- 0x17
+  Internet Address      Physical Address      Type
+  10.5.255.255                                static
+  224.0.0.22                                  static
+  224.0.0.251                                 static
+  224.0.0.252                                 static
+  224.0.0.253                                 static
+  239.255.255.250                             static
+Mizutani
+
+ — 27/12/2025, 14:49
+Ethernet adapter Ethernet:
+
+   Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::c341:af9b:9e35:98ce%4
+   Autoconfiguration IPv4 Address. . : 169.254.159.161
+   Subnet Mask . . . . . . . . . . . : 255.255.0.0
+   Default Gateway . . . . . . . . . :
+Ethernet adapter Ethernet:
+
+   Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::c341:af9b:9e35:98ce%4
+   IPv4 Address. . . . . . . . . . . : 192.168.137.1
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . :
+alt — 27/12/2025, 15:29
+Set-NetConnectionProfile -Name "NetworkName" -NetworkCategory Private
+alt — 03/01/2026, 10:53
+I'm lowjey jus your lil storage
+Mizutani
+
+ — 03/01/2026, 22:17
+esmBot
+APP
+ — 03/01/2026, 22:17
+Image
+Mizutani
+
+ — 04/01/2026, 17:26
+Image
+Mizutani
+ started a call that lasted a few seconds. — 04/01/2026, 23:49
+alt — 07/01/2026, 12:20
+Attachment file type: code
+Data types in R.R
+622 bytes
+Attachment file type: code
+Factor in R.R
+214 bytes
+Attachment file type: code
+Matrix function in R.R
+603 bytes
+Mizutani
+
+ — 10/01/2026, 14:31
+Image
+Image
+Mizutani
+
+ — 10/01/2026, 17:53
+Image
+Image
+Image
+Image
+Image
+Image
+Image
+alt — 10/01/2026, 19:40
+Image
+alt — 10/01/2026, 19:51
+Image
+Image
+alt — 10/01/2026, 20:31
+Attachment file type: unknown
+CamCapture1.pcapng
+7.97 MB
+Attachment file type: unknown
+Capture1.pcapng
+4.41 MB
+Attachment file type: unknown
+Capture2.pcapng
+1.50 KB
+Attachment file type: unknown
+Capture3.pcapng
+7.46 MB
+Attachment file type: unknown
+Capture4.pcapng
+4.09 MB
+Attachment file type: unknown
+Capture5.pcapng
+8.14 MB
+Attachment file type: unknown
+init capture.pcapng
+189.74 KB
+Attachment file type: unknown
+JECapture1.pcapng
+5.97 MB
+Attachment file type: unknown
+JECapture2.pcapng
+6.83 MB
+Attachment file type: unknown
+JECapture3.pcapng
+4.86 MB
+Mizutani
+
+ — 10/01/2026, 22:27
+Filtering for 'beacon frames'
+Image
+Beacon frames + my wifi network's name
+Image
+The MAC addresses of my house are:
+AA:95:7F:2D:AE:AC
+AA:95:7F:2D:AD:38
+AA:95:7F:2D:AB:34
+Mizutani
+
+ — 10/01/2026, 22:52
+Found a MAC address that Wireshark's local OUI registry resolved to Google
+Image
+Mizutani
+
+ — 11/01/2026, 01:37
+What we can see here:
+Periodic data from camera
+Around packet 25k, I power cycled the camera
+We can see probe requests as the device attempts to connect to the internet again followed by data as I am actively viewing the camera via the app
+Image
+Mizutani
+
+ — 13/01/2026, 21:08
+Image
+Mizutani
+
+ — 13/01/2026, 23:19
+Image
+Mizutani
+
+ — 14/01/2026, 19:57
+Image
+Mizutani
+
+ — 14/01/2026, 23:51
+Image
+Mizutani
+
+ — 19/01/2026, 22:56
+Image
+Mizutani
+
+ — 19/01/2026, 23:08
+Image
+Mizutani
+
+ — 19/01/2026, 23:26
+Image
+Mizutani
+
+ — 22/01/2026, 03:12
+Image
+esmBot
+APP
+ — 22/01/2026, 20:14
+Image
+esmBot
+APP
+ — 24/01/2026, 13:53
+Image
+Mizutani
+
+ — 24/01/2026, 22:40
+Done! Congratulations on your new bot. You will find it at t.me/RacheBartmoss_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+
+Use this token to access the HTTP API:
+8470318862:AAEjQMdW9YMurwHSv5uu9cmdJ9F_HuSc76c
+Keep your token secure and store it safely, it can be used by anyone to control your bot.
+
+For a description of the Bot API, see this page: https://core.telegram.org/bots/api
+Telegram Bot API
+The Bot API is an HTTP-based interface created for developers keen on building bots for Telegram.
+To learn how to create…
+Mizutani
+
+ — 26/01/2026, 01:48
+To be totally honest I wasn't even going to respond but I found a screenshot of the last time you've pulled this shit and I cannot believe I still trusted you after the first time. I cannot count how many times you've told me you're some innocent bystander only to go ahead and tell someone what I've said behind my back. You did it with Allicia with the tents and you've done it with Anya countless times. I felt like I could trust you and vent to you to get something off my chest but you took everything I said literally and used it as ammo to fuel this shit. I never should have trusted you again after the first time and especially after I explained how Jamie did the exact same shit and you still went ahead and did it. Every single time you told someone something I said in confidence, you made it infinitely worse. I just cannot understand it at all man. I'm so fucking disappointed in you.
+alt — Yesterday at 20:39
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+configuration.nix
+5 KB
+{
+  "nodes": {
+    "home-manager": {
+      "inputs": {
+        "nixpkgs": [
+          "nixpkgs"
+
+flake.lock
+2 KB
+{
+    description = "NixOS config with Home Manager via flakes";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+
+flake.nix
+1 KB
+# Do not modify this file!  It was generated by ‘nixos-generate-config’
+# and may be overwritten by future invocations.  Please make changes
+# to /etc/nixos/configuration.nix instead.
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+
+hardware-configuration.nix
+2 KB
+{
+    home.stateVersion = "25.05";
+}
+home.nix
+1 KB
+Mizutani
+
+ — Yesterday at 22:12
+Here is the token for bot Rache @RacheBartmoss_bot:
+
+8470318862:AAEjQMdW9YMurwHSv5uu9cmdJ9F_HuSc76c
+alt — 13:30
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+configuration.nix
+5 KB
+{
+  "nodes": {
+    "home-manager": {
+      "inputs": {
+        "nixpkgs": [
+          "nixpkgs"
+
+flake.lock
+2 KB
+{
+    description = "NixOS config with Home Manager via flakes";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+
+flake.nix
+1 KB
+# Do not modify this file!  It was generated by ‘nixos-generate-config’
+# and may be overwritten by future invocations.  Please make changes
+# to /etc/nixos/configuration.nix instead.
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+
+hardware-configuration.nix
+2 KB
+{
+    home.stateVersion = "25.05";
+}
+home.nix
+1 KB
+﻿
+alt
+basedgroovy
+him
+ 
+# Do not modify this file!  It was generated by ‘nixos-generate-config’
+# and may be overwritten by future invocations.  Please make changes
+# to /etc/nixos/configuration.nix instead.
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/b5791c5a-bd09-4b7a-bed5-265bc73a073e";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/14F5-DD71";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/ce92e13d-cad1-431c-9ee7-72cb2d8a5df7"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wwp0s20u4i6.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
+hardware-configuration.nix
+2 KB
